743f6ae6b0b819de561c99f14cf14e35052abf79
evanleung23 <71602688+evanleung23@users.noreply.github.com>
2021-04-27 21:50:22 -0400

    revisions day 1

--- src/js/index.js
+++ src/js/index.js
@@ -1,111 +1,47 @@
 console.log("Activated");
-const buttons1ID = document.getElementById("buttons1");
-const buttons2ID = document.getElementById("buttons2");
-const buttons3ID = document.getElementById("buttons3");
-const buttons4ID = document.getElementById("buttons4");
-const startButton = document.getElementById("startbtn");
-const nextButton = document.getElementById("submitbtn");
-const containerID = document.getElementById("container");
+const startbutton = document.getElementById("startbtn");
+const container = document.getElementById("container");
+const question = document.getElementById("question");
+const buttons = document.getElementById("buttons");
 const scoreboardID = document.getElementById("scoreboard");
 
-const btn11ID = document.getElementById("btn11");
-const btn12ID = document.getElementById("btn12");
-const btn13ID = document.getElementById("btn13");
-const btn14ID = document.getElementById("btn14");
-const btn21ID = document.getElementById("btn21");
-const btn22ID = document.getElementById("btn22");
-const btn23ID = document.getElementById("btn23");
-const btn24ID = document.getElementById("btn24");
-const btn31ID = document.getElementById("btn31");
-const btn32ID = document.getElementById("btn32");
-const btn33ID = document.getElementById("btn33");
-const btn34ID = document.getElementById("btn34");
-const btn41ID = document.getElementById("btn41");
-const btn42ID = document.getElementById("btn42");
-const btn43ID = document.getElementById("btn43");
-const btn44ID = document.getElementById("btn44");
+let button1 = document.getElementById("btn1");
+let button2 = document.getElementById("btn2");
+let button3 = document.getElementById("btn3");
+let button4 = document.getElementById("btn4");
 
-startButton.addEventListener("click", gameStart);
+const questions = ["Question 1", "Question 2"];
+const correctAnswers = ["Answer 1", "Answer7"];
+const wrongAnswers = [
+  "Answer 2",
+  "Answer 3",
+  "Answer 4",
+  "Answer 5",
+  "Answer 6",
+  "Answer 8",
+];
 
-let quizscore = 0;
+startbutton.addEventListener("click", startGame);
+let quizScore = 0;
 
-function gameStart() {
-  startButton.classList.add("remove");
-  containerID.classList.remove("remove");
+function startGame() {
+  startbutton.classList.add("remove");
+  container.classList.remove("remove");
 }
 
-btn11ID.addEventListener("click", question1correct);
-btn12ID.addEventListener("click", question1wrong);
-btn13ID.addEventListener("click", question1wrong);
-btn14ID.addEventListener("click", question1wrong);
+buttons.addEventListener("click", scoreKeeper, nextQuestion);
 
-function question1correct() {
-  quizscore++;
-  buttons1ID.classList.add("remove");
-  scoreboardID.innerText = quizscore;
-  console.log(quizscore);
+function scoreKeeper(e) {
+  if ((e.target.parent = correctAnswers)) {
+    quizScore++;
+    scoreboardID.innerText = quizScore;
+  } else {
+    quizScore;
+    scoreboardID.innerText = quizScore;
+  }
 }
 
-function question1wrong() {
-  quizscore;
-  buttons1ID.classList.add("remove");
-  scoreboardID.innerText = quizscore;
-  console.log(quizscore);
-}
-
-btn21ID.addEventListener("click", question2wrong);
-btn22ID.addEventListener("click", question2wrong);
-btn23ID.addEventListener("click", question2correct);
-btn24ID.addEventListener("click", question2wrong);
-
-function question2correct() {
-  quizscore++;
-  buttons2ID.classList.add("remove");
-  scoreboardID.innerText = quizscore;
-  console.log(quizscore);
-}
-
-function question2wrong() {
-  quizscore;
-  buttons2ID.classList.add("remove");
-  scoreboardID.innerText = quizscore;
-  console.log(quizscore);
-}
-
-btn31ID.addEventListener("click", question3wrong);
-btn32ID.addEventListener("click", question3wrong);
-btn33ID.addEventListener("click", question3wrong);
-btn34ID.addEventListener("click", question3correct);
-
-function question3correct() {
-  quizscore++;
-  buttons3ID.classList.add("remove");
-  scoreboardID.innerText = quizscore;
-  console.log(quizscore);
-}
-
-function question3wrong() {
-  quizscore;
-  buttons3ID.classList.add("remove");
-  scoreboardID.innerText = quizscore;
-  console.log(quizscore);
-}
-
-btn41ID.addEventListener("click", question4wrong);
-btn42ID.addEventListener("click", question4wrong);
-btn43ID.addEventListener("click", question4correct);
-btn44ID.addEventListener("click", question4wrong);
-
-function question4correct() {
-  quizscore++;
-  buttons4ID.classList.add("remove");
-  scoreboardID.innerText = quizscore;
-  console.log(quizscore);
-}
-
-function question4wrong() {
-  quizscore;
-  buttons4ID.classList.add("remove");
-  scoreboardID.innerText = quizscore;
-  console.log(quizscore);
+function nextQuestion(theQuestion, answer1, answer2, answer3, answer4) {
+  htmlButtons = document.querySelector(".container");
+  console.log(htmlButtons);
 }
---
+++ src/js/questions.js
@@ -0,0 +1,5 @@
+export const questions;
+const questions = [
+  (question1 = ["Question 1", "Answer 1", "Answer 2", "Answer 3", "Answer 4"]),
+  (question2 = ["Question 2", "Choice 1", "Choice 2", "Choice 3", "Choice 4"]),
+];
